5a38324816ee00898850eb4318015c0efb48489f
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-22 01:59:32 +0900

    섹션2. JPA 시작하기

---
+++ Java/JPA/JPAStudy/hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,28 @@
+package hello.hellospring.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //identity란 DB에서 자동으로 생성해주는 값
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
