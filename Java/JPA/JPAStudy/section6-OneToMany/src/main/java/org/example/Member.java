3e8debe598009dfc35e73b4f13b31b83b5534d81
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-22 15:17:54 +0900

    일대다 연관관계 공부 기록

---
+++ Java/JPA/JPAStudy/section6-OneToMany/src/main/java/org/example/Member.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    // JoinColumn에서 name만 적으면 서로 주인관계가 되기 떄문에 망한다.
+    // insertable,updatable을 false로 두면 Team team은 읽기전용매핑이 된다.
+    //어찌됐든 다대일 양방향을 사용하자.
+    @JoinColumn(name = "TEAM_ID",insertable = false,updatable = false)
+    private Team team;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+
+
+    }
