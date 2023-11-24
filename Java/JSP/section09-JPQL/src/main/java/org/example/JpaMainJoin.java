7304f4ac76636341486bdb9e28f7dd6c926dbc5d
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-24 18:25:02 +0900

    조인 쿼리 공부 기록

--- Java/JSP/section09-JPQL/src/main/java/org/example/JpaMainJoin.java
+++ Java/JSP/section09-JPQL/src/main/java/org/example/JpaMainJoin.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class JpaMainPaging {
+public class JpaMainJoin {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -17,27 +17,33 @@ public class JpaMainPaging {
         tx.begin();
         try{
 
-            for( int i=0 ; i<100; i++){
+                Team team = new Team();
+                team.setName("team");
+                em.persist(team);
+
             Member member = new Member();
-            member.setUsername("member"+i);
-            member.setAge(i);
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+
             em.persist(member);
-            }
 
             em.flush();
             em.clear();
 
-            //paging에서 orderby를 꼭 넣어야 순서대로 가져와지기 떄문에 필수다.
-            //최소 최대값만 정해주면 지정 범위의 인덱스 값을 가져와준다.
-            List<Member> resultList = em.createQuery("select m from Member  m order by m.age", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            //inner, outer는 생략 가능
+            String query = "select m from Member  m  join m.team t where m.username = t.name";
+            List<Member> resultList = em.createQuery(query, Member.class)
+                    .getResultList();
+            System.out.println("resultList.size() = " + resultList.size());
+
+
+            System.out.println("====start=====");
+            String query2 = "select m from Member  m  left join m.team t on t.name = temaA";
+            List<Member> resultList2 = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result.size =" + resultList.size());
-            for (Member member : resultList) {
-                System.out.println(member);
-            }
+            System.out.println("resultList2 = " + resultList2.size());
 
 
 
