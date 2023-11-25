7530eb4d7c8122aa825eb41747fcbe7e97a00bf6
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-25 23:44:07 +0900

    Delivery 객체 생성

--- Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -2,9 +2,21 @@ package jpabook.jpashop.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "order")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    private DeliveryStatus status;
 }
