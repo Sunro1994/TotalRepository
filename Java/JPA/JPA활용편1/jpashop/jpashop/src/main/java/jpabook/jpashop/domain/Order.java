b957ee9f87d6ea3acf298363a82e6356be4b918e
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-25 23:40:58 +0900

    Order 클래스

---
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member") //해당 테이블에대해 맵핑될 뿐, 검색 전용
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+    @OneToOne
+    private Delivery delivery;
+
+
+    private LocalDateTime orderDate; //hibernate가 알아서 @Temporal 타입을 맞춰준다.
+
+
+    private OrderStatus status; //주문 상태 [Order,Cancle]
+}
