62eae03fe3b47656f55fc82175a40efce1422f0f
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-25 23:39:36 +0900

    앨범 (Item 상속 클래스)

---
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class Album extends  Item{
+
+    private String artist;
+    private String etc;
+}
