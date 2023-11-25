48a194932eb61f20804e62e1f54981af6e40dbb9
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-25 23:39:42 +0900

    북 (Item 상속 클래스)

---
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class Book extends  Item{
+
+    private String author;
+    private int isbn;
+
+}
