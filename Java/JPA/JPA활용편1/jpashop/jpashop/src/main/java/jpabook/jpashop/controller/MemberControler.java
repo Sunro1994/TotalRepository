5faf4e595f4b8e9bd7dfc6314ff0085a46ec0d86
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-01 22:56:11 +0900

    회원 목록 출력 추가

--- Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberControler.java
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberControler.java
@@ -1,10 +1,17 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,7 +21,35 @@ public class MemberControler {
 
     @GetMapping("/members/new")
     public String createForm(Model model) {
-        model.addAttribute("memberFOrm", new MemberForm());
+        model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
+
+    @PostMapping("/members/new")
+    //valid는 해당 폼에 있는 값들의 유효성을 검사해준다.
+    //BindingResult는 SpringBoot에서 제공하는 클래스, 에러가 생긴경우 자동으로 다른페이지로 이동시켜준다.
+    //MemberForm으로 받는 이유 Member에 NotEmpty같은 어노테이션을 추구하면 지저분해지고, 받아오는 값을 정확히 알 수 없음
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
+
--- Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class MemberForm {
 
+    @NotEmpty(message = "회원 이름은 필수 입니다.") //값이 비어있으면 오류 메세지 출력
     private String name;
 
-    @NotEmpty(message = "회원 이름은 필수 입니다.") //값이 비어있으면 오류 메세지 출력
     private String city;
     private String street;
     private String zipcode;
