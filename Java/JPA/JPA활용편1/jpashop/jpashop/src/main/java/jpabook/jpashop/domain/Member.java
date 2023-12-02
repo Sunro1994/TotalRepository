5cbd171d341f868afcad9e4b5a61c5d8bb9043fc
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-02 18:27:42 +0900

    null체크를 위한 NotEmpty 어노테이션과 JSON객체로 반환시 무시할 필드 @JsonIgnore 적용

--- Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ Java/JPA/JPA활용편1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +16,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
