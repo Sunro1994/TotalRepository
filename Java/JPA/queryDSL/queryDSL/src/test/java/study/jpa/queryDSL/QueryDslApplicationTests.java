4a05fb67fb45d0dc8ba76d0128e084d7a3feb41c
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-08 14:56:01 +0900

    queryDSL 파일 생성

---
+++ Java/JPA/queryDSL/queryDSL/src/test/java/study/jpa/queryDSL/QueryDslApplicationTests.java
@@ -0,0 +1,34 @@
+package study.jpa.queryDSL;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.jpa.queryDSL.domain.Hello;
+import study.jpa.queryDSL.domain.QHello;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class QueryDslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		Assertions.assertThat(result).isEqualTo(hello);
+
+		//lombok 동작 확인 (hello.getId())
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+}
