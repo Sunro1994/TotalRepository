918fead7885687506b4c89ec32f6caa6c8cf9ac7
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-08 17:56:52 +0900

    상수 및 숫자 합치기

--- Java/JPA/queryDSL/queryDSL/src/test/java/study/jpa/queryDSL/QueryDslBasicTest.java
+++ Java/JPA/queryDSL/queryDSL/src/test/java/study/jpa/queryDSL/QueryDslBasicTest.java
@@ -2,6 +2,8 @@ package study.jpa.queryDSL;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -420,6 +422,69 @@ public class QueryDslBasicTest{
     }
 
 
+    @Test
+    public void basicCase() {
+        List<String> fetch = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        for (String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
+
+
+        @Test
+        public void complexCase(){
+        //DB가 아닌 application에서 로직을 통해 바꾸거나, 프레젠테이션에서 로직을 짜야 한다.
+            List<String> fetch = queryFactory
+                    .select(new CaseBuilder()
+                            .when(member.age.between(0, 20)).then("0~20살")
+                            .when(member.age.between(21, 30)).then("21~40살")
+                            .otherwise("기타")
+                    ).from(member)
+                    .fetch();
+
+            for (String s : fetch) {
+                System.out.println("s = " + s);
+            }
+
+
+        }
+
+
+        @Test
+    public void constant() {
+            List<Tuple> result = queryFactory
+                    .select(member.username, Expressions.constant("A"))
+                    .from(member)
+                    .fetch();
+
+            for (Tuple tuple : result) {
+                System.out.println("tuple = " + tuple);
+            }
+        }
+
+        @Test
+    public void concat(){
+        //username_age만들기
+            List<String> result = queryFactory
+                    .select(member.username.concat("_").concat(member.age.stringValue()))
+                    .from(member)
+                    .where(member.username.eq("member1"))
+                    .fetch();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+        }
+
 
     }
+
+
+
 
