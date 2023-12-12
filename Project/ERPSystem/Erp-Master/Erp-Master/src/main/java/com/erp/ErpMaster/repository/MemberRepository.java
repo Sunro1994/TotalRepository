1e9b5e9ee46e8f54e462e3890e60b3d244eac843
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-12 10:48:13 +0900

    MemberRepository 적용

---
+++ Project/ERPSystem/Erp-Master/Erp-Master/src/main/java/com/erp/ErpMaster/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.erp.ErpMaster.repository;
+
+import com.erp.ErpMaster.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByLoginId(String username);
+}
