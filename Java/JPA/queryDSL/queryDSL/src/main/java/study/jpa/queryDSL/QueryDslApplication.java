26dc3103703fd3eb421b440b158ce8ca4e914d4a
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-13 14:30:36 +0900

    JPAQueryFactory 의존성 외부 주입

--- Java/JPA/queryDSL/queryDSL/src/main/java/study/jpa/queryDSL/QueryDslApplication.java
+++ Java/JPA/queryDSL/queryDSL/src/main/java/study/jpa/queryDSL/QueryDslApplication.java
@@ -1,7 +1,11 @@
 package study.jpa.queryDSL;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QueryDslApplication {
@@ -10,4 +14,9 @@ public class QueryDslApplication {
 		SpringApplication.run(QueryDslApplication.class, args);
 	}
 
+	//외부에서 주입받는 방식으로 의존성이 생길 수 있다.
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
 }
