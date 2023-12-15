2f1d56fe5671cdc0380211ba7aa5616ece7fab04
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-16 00:29:18 +0900

    암호 문제풀이 추가

--- Java/CordingTest/RecordTest/src/test/java/CordTest.java
+++ Java/CordingTest/RecordTest/src/test/java/CordTest.java
@@ -173,21 +173,32 @@ public class CordTest {
             }
 
         }
-
-
-
-
         String answer ="";
         for (String i : arr) {
             answer += i;
         }
         System.out.println(answer);
-
-
-
-
-
     }
 
+    /**
+     * 1. “#*****#”를 일곱자리의 이진수로 바꿉니다. #은 이진수의 1로, *이진수의 0으로 변환합니다.
+     * 결과는 “1000001”로 변환됩니다.
+     *
+     * 2. 바뀐 2진수를 10진수화 합니다. “1000001”을 10진수화 하면 65가 됩니다.
+     *
+     * 3. 아스키 번호가 65문자로 변환합니다. 즉 아스크번호 65는 대문자 'A'입니다.
+     */
+    @Test
+    public void password(){
+        int cnt =4;
+        String pw = "#****###**#####**#####**##**".replaceAll("#","1").replace("*","0");
+        String answer = "";
+
+        for(int i=0 ; i<cnt*7 ; i+=7){
+            char target =(char)Integer.parseInt(pw.substring(i,i+7),2);
+            answer +=target;
+        }
+        System.out.println("answer = " + answer);
+    }
 
 }
