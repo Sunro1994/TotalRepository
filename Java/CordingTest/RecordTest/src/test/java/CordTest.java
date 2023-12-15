631e98298a24c74384573eeea49dde8eca1eaa55
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-16 01:41:56 +0900

    폴더 이름 BackEnd로 변경

--- Java/CordingTest/RecordTest/src/test/java/CordTest.java
+++ Java/CordingTest/RecordTest/src/test/java/CordTest.java
@@ -202,25 +202,71 @@ public class CordTest {
         System.out.println("answer = " + answer);
     }
 
+    /**
+     * N개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작성하세요.
+     *
+     * (첫 번째 수는 무조건 출력한다)
+     *
+     *
+     * 입력
+     * 첫 줄에 자연수 N(1<=N<=100)이 주어지고, 그 다음 줄에 N개의 정수가 입력된다.
+     */
     @Test
     @DisplayName("Scanner는 작동되지 않으니 임의의 수를 넣어줘야 한다.")
-    public void pringBigNumber(){
+    public void pringBigNumber() {
         Scanner sc = new Scanner(System.in);
         int len = sc.nextInt();
         List<Integer> list = new ArrayList<>();
 
-        for(int i=0; i<len;i++){
+        for (int i = 0; i < len; i++) {
             list.add(sc.nextInt());
         }
 
-        for(int i=0; i<list.size();i++){
-            if(i==0) System.out.print(list.get(i)+" ");
-            else if(list.get(i)>list.get(i-1)){
-                System.out.print(list.get(i)+" ");
+        for (int i = 0; i < list.size(); i++) {
+            if (i == 0) System.out.print(list.get(i) + " ");
+            else if (list.get(i) > list.get(i - 1)) {
+                System.out.print(list.get(i) + " ");
             }
+        }
+    }
+
+    /**
+     * 선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
+     *
+     * 선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+     *
+     * 첫 줄에 정수 N(5<=N<=100,000)이 입력된다. 그 다음줄에 N명의 학생의 키가 앞에서부터 순서대로 주어진다.
+     */
+        @Test
+        public void canSeeStudent(){
+            Scanner sc = new Scanner(System.in);
+            int len = sc.nextInt();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i=0; i<len;i++){
+                list.add(sc.nextInt());
+            }
+
+            int cnt =0;
+            int maxHeight=0;
+            for(int i=0; i<list.size();i++){
+                if(i==0) {
+                    cnt++;
+                    maxHeight = list.get(i);
+                    continue;
+                }
+                if(list.get(i)>maxHeight){
+                    cnt++;
+                    maxHeight = list.get(i);
+                }
+
+
+            }
+            System.out.println(cnt);
+
         }
 
 
     }
 
-}
+
