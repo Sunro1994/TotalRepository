b901af187bc9914b81acc85beee630747af605a4
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-18 02:20:51 +0900

    RestController 구현 API 통신 정상작동

---
+++ Project/ERPSystem/BackEnd/Erp-Master/src/main/java/com/erp/ErpMaster/login/LoginRestController.java
@@ -0,0 +1,27 @@
+package com.erp.ErpMaster.login;
+
+import com.erp.ErpMaster.member.Member;
+import com.erp.ErpMaster.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class LoginRestController {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PostMapping("/api/login")
+    public Long login(@RequestBody Map<String,String> params) {
+        Member member = memberRepository.findByLoginId(params.get("userid"));
+        if (member != null) {
+            return member.getId();
+        }
+        return 0L;
+    }
+}
