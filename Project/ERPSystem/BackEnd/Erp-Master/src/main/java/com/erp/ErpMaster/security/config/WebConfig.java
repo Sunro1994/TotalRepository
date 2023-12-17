79d7fae3411ef7c010a9c1d7c6e6cb1c48724b46
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-18 02:20:28 +0900

    Back-end CORS 정책 Config 파일

---
+++ Project/ERPSystem/BackEnd/Erp-Master/src/main/java/com/erp/ErpMaster/security/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.erp.ErpMaster.security.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 허용할 오리진 (모두 허용할 경우 "*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
