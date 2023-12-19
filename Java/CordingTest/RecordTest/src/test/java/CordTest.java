0a21175379c66a31cd863822cc379badc3472e88
Sunro Lee <tjsfhdhkd@naver.com>
2023-12-19 09:37:32 +0900

    뒤집은숫자는 소수인가?, 순위 구하기, 점수 누적 문제 풀이

--- Java/CordingTest/RecordTest/src/test/java/CordTest.java
+++ Java/CordingTest/RecordTest/src/test/java/CordTest.java
@@ -12,12 +12,12 @@ public class CordTest {
 
     @Test
     //중복문자제거
-    public void deleteString(){
+    public void deleteString() {
         String target = "ksekkset";
         String answer = "";
 
         List<Character> arr = new ArrayList<>();
-        for(int i=0; i<target.length(); i++){
+        for (int i = 0; i < target.length(); i++) {
             arr.add(target.charAt(i));
         }
 
@@ -33,11 +33,11 @@ public class CordTest {
 
     @Test
     //중복문자제거2
-    public void deleteString2(){
+    public void deleteString2() {
         String target = "ksekkset";
         String answer = "";
-        for (int i=0; i<target.length();i++){
-            if (target.indexOf(target.charAt(i))==i) {
+        for (int i = 0; i < target.length(); i++) {
+            if (target.indexOf(target.charAt(i)) == i) {
                 answer += target.charAt(i);
             }
 
@@ -49,7 +49,7 @@ public class CordTest {
     @Test
     //회문문자열 판단
     //IgnoreCase 대소문자를 무시하고 비교한다.
-    public void reverseString(){
+    public void reverseString() {
         String target = "gooG";
         target = target;
         StringBuffer sb = new StringBuffer(target);
@@ -62,7 +62,7 @@ public class CordTest {
     }
 
     @Test
-    public void replaceAll(){
+    public void replaceAll() {
         String target = "found7, time: study; Yduts; emit, 7Dnuof\n";
         //repalce에서는 정규식을 사용할 수 없다.
         // ^A-Z : A~Z가 아닌것들
@@ -74,7 +74,7 @@ public class CordTest {
     }
 
     @Test
-    public void ExtractOnlyNumber(){
+    public void ExtractOnlyNumber() {
         String target = "g0en2T0s8eSoft";
         System.out.println((int) '0'); //48
         System.out.println((int) '9'); //57
@@ -82,8 +82,8 @@ public class CordTest {
         int answer = Integer.parseInt(target.replaceAll("[^0-9]", ""));
 
         String answer2 = "";
-        for (char x :target.toCharArray()) {
-            if(Character.isDigit(x)) answer2 += x;
+        for (char x : target.toCharArray()) {
+            if (Character.isDigit(x)) answer2 += x;
         }
         System.out.println(answer);
         System.out.println(Integer.parseInt(answer2));
@@ -96,7 +96,7 @@ public class CordTest {
      */
 
     @Test
-    public void shortestStringLnegth(){
+    public void shortestStringLnegth() {
         Scanner sc = new Scanner(System.in);
 
 
@@ -106,26 +106,24 @@ public class CordTest {
         int[] arr = new int[a.length()];
 
         int p = 1000;
-        for(int i=0; i<a.length();i++){
-            if(b.equals(a.charAt(i)+"")){
-                p=0;
-                arr[i]=p;
-            }
-            else{
+        for (int i = 0; i < a.length(); i++) {
+            if (b.equals(a.charAt(i) + "")) {
+                p = 0;
+                arr[i] = p;
+            } else {
                 p++;
-                arr[i]=p;
+                arr[i] = p;
             }
         }
 
         p = 1000;
         for (int i = a.length() - 1; i >= 0; i--) {
-            if(b.equals(a.charAt(i)+"")){
-                p=0;
-                arr[i]=p;
-            }
-            else{
+            if (b.equals(a.charAt(i) + "")) {
+                p = 0;
+                arr[i] = p;
+            } else {
                 p++;
-                arr[i]=Math.min(arr[i],p);
+                arr[i] = Math.min(arr[i], p);
             }
         }
 
@@ -136,45 +134,43 @@ public class CordTest {
         sc.close();
 
 
-
     }
 
 
     /**
      * 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
-     *
+     * <p>
      * 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
      */
 
     @Test
-    public void compactString(){
+    public void compactString() {
         String x = "KKTFFFFFFEEE";
 
         char target = x.charAt(0);
         List<String> arr = new ArrayList<>();
 
-        int count =1;
+        int count = 1;
         for (int i = 0; i < x.length(); i++) {
-            if(i==0) arr.add(target+"");
+            if (i == 0) arr.add(target + "");
 
-            else if(x.charAt(i)==target){
+            else if (x.charAt(i) == target) {
                 count++;
-                if(i==x.length()-1){
-                    arr.add(count+"");
+                if (i == x.length() - 1) {
+                    arr.add(count + "");
                 }
-            }
-            else{
-                if(count!=1){
-                arr.add(count+"");
+            } else {
+                if (count != 1) {
+                    arr.add(count + "");
 
                 }
                 target = x.charAt(i);
-                arr.add(target+"");
-                count =1;
+                arr.add(target + "");
+                count = 1;
             }
 
         }
-        String answer ="";
+        String answer = "";
         for (String i : arr) {
             answer += i;
         }
@@ -184,30 +180,30 @@ public class CordTest {
     /**
      * 1. “#*****#”를 일곱자리의 이진수로 바꿉니다. #은 이진수의 1로, *이진수의 0으로 변환합니다.
      * 결과는 “1000001”로 변환됩니다.
-     *
+     * <p>
      * 2. 바뀐 2진수를 10진수화 합니다. “1000001”을 10진수화 하면 65가 됩니다.
-     *
+     * <p>
      * 3. 아스키 번호가 65문자로 변환합니다. 즉 아스크번호 65는 대문자 'A'입니다.
      */
     @Test
-    public void password(){
-        int cnt =4;
-        String pw = "#****###**#####**#####**##**".replaceAll("#","1").replace("*","0");
+    public void password() {
+        int cnt = 4;
+        String pw = "#****###**#####**#####**##**".replaceAll("#", "1").replace("*", "0");
         String answer = "";
 
-        for(int i=0 ; i<cnt*7 ; i+=7){
-            char target =(char)Integer.parseInt(pw.substring(i,i+7),2);
-            answer +=target;
+        for (int i = 0; i < cnt * 7; i += 7) {
+            char target = (char) Integer.parseInt(pw.substring(i, i + 7), 2);
+            answer += target;
         }
         System.out.println("answer = " + answer);
     }
 
     /**
      * N개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작성하세요.
-     *
+     * <p>
      * (첫 번째 수는 무조건 출력한다)
-     *
-     *
+     * <p>
+     * <p>
      * 입력
      * 첫 줄에 자연수 N(1<=N<=100)이 주어지고, 그 다음 줄에 N개의 정수가 입력된다.
      */
@@ -232,85 +228,86 @@ public class CordTest {
 
     /**
      * 선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
-     *
+     * <p>
      * 선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
-     *
+     * <p>
      * 첫 줄에 정수 N(5<=N<=100,000)이 입력된다. 그 다음줄에 N명의 학생의 키가 앞에서부터 순서대로 주어진다.
      */
-        @Test
-        public void canSeeStudent(){
-            Scanner sc = new Scanner(System.in);
-            int len = sc.nextInt();
-            List<Integer> list = new ArrayList<>();
+    @Test
+    public void canSeeStudent() {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
 
-            for(int i=0; i<len;i++){
-                list.add(sc.nextInt());
+        for (int i = 0; i < len; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int cnt = 0;
+        int maxHeight = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (i == 0) {
+                cnt++;
+                maxHeight = list.get(i);
+                continue;
+            }
+            if (list.get(i) > maxHeight) {
+                cnt++;
+                maxHeight = list.get(i);
             }
 
-            int cnt =0;
-            int maxHeight=0;
-            for(int i=0; i<list.size();i++){
-                if(i==0) {
-                    cnt++;
-                    maxHeight = list.get(i);
-                    continue;
-                }
-                if(list.get(i)>maxHeight){
-                    cnt++;
-                    maxHeight = list.get(i);
-                }
-
-
-            }
-            System.out.println(cnt);
 
         }
+        System.out.println(cnt);
+
+    }
 
     /**
      * 가위 바위 보
      */
 
     @Test
-    public void RockSeizerPepper(){
-            Scanner sc = new Scanner(System.in);
-            int cnt = sc.nextInt();
-            int[] userA = new int[cnt];
-            int[] userB = new int[cnt];
+    public void RockSeizerPepper() {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] userA = new int[cnt];
+        int[] userB = new int[cnt];
 
-            for (int i = 0; i < cnt; i++) {
-                userA[i] = sc.nextInt();
-            }
-
-            for (int i = 0; i < cnt; i++) {
-                userB[i] = sc.nextInt();
-            }
-            for (int i = 0; i < cnt; i++) {
-                if(userA[i] == userB[i]) System.out.println("D");
-                else if(userA[i]==1 && userB[i]==3) System.out.println("A");
-                else if(userA[i]==2 && userB[i]==1) System.out.println("A");
-                else if(userA[i]==3 && userB[i]==2) System.out.println("A");
-                else System.out.println("B");
-            }
-
-
+        for (int i = 0; i < cnt; i++) {
+            userA[i] = sc.nextInt();
         }
+
+        for (int i = 0; i < cnt; i++) {
+            userB[i] = sc.nextInt();
+        }
+        for (int i = 0; i < cnt; i++) {
+            if (userA[i] == userB[i]) System.out.println("D");
+            else if (userA[i] == 1 && userB[i] == 3) System.out.println("A");
+            else if (userA[i] == 2 && userB[i] == 1) System.out.println("A");
+            else if (userA[i] == 3 && userB[i] == 2) System.out.println("A");
+            else System.out.println("B");
+        }
+
+
+    }
+
     /**
      * 피보나치 수열 계산법
      */
-@Test
-    public void fibonacciSequence(){
+    @Test
+    public void fibonacciSequence() {
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
-        int[] arr = new int[cnt+1];
-        for (int i = 1; i <=cnt; i++) {
-            if(i==1 || i==2)  arr[i] = 1;
+        int[] arr = new int[cnt + 1];
+        for (int i = 1; i <= cnt; i++) {
+            if (i == 1 || i == 2) arr[i] = 1;
             else {
                 arr[i] = arr[i - 1] + arr[i - 2];
             }
         }
         for (int i : arr) {
-            if(i==0) continue;
-            System.out.print(i+" ");
+            if (i == 0) continue;
+            System.out.print(i + " ");
         }
     }
 
@@ -319,16 +316,16 @@ public class CordTest {
      * 에라토스테네스 체 - 소수 구하기
      */
 
-    public void Decimal(){
+    public void Decimal() {
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
         int result = 0;
-        int[] arr = new int[cnt+1];
-        for(int i=2; i<=cnt;i++){
+        int[] arr = new int[cnt + 1];
+        for (int i = 2; i <= cnt; i++) {
             if (arr[i] == 0) {
                 result++;
                 for (int j = i; j <= cnt; j = j + i) {
-                    arr[j]=1;
+                    arr[j] = 1;
                 }
             }
         }
@@ -336,6 +333,96 @@ public class CordTest {
 
     }
 
+
+    @Test
+    public void reverseNumIsDecimal() {
+        /**
+         *  1. 반복수 입력
+         *  2. 반복 수 만큼 배열에 삽입
+         *  3. 숫자 뒤집기
+         *  4. 소수인지 판별
+         */
+        Scanner sc = new Scanner(System.in);
+        //        1. 반복수 입력
+        int cnt = sc.nextInt();
+        List<Integer> arr = new ArrayList<>();
+//        2. 반복 수 만큼 배열에 삽입
+        for (int i = 0; i < cnt; i++) {
+//        3. 숫자 뒤집기
+            String s = sc.next();
+            StringBuilder sb = new StringBuilder(s);
+            int target = Integer.parseInt(sb.reverse().toString());
+            System.out.println("target = " + target);
+            if (isDecimal(target) && target > 1) arr.add(target);
+        }
+
+        arr.forEach(i -> System.out.print(i + " "));
+    }
+
+    private static Boolean isDecimal(int target) {
+        int cnt = 1;
+        for (int i = 2; i <= target / 2; i++) {
+            System.out.println("target%" + i + "= " + target % i);
+            if (target % i == 0) {
+                cnt++;
+            }
+            if (cnt > 1) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    @Test
+    public void getScore(){
+        /**
+         * 1. 1이면 점수 증가
+         * 2. 연속으로 1이 나오면 득점 수 +1
+         * 3. 틀리면 0으로 초기화
+         */
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        Boolean flag = true;
+        int score = 0;
+        int result =0;
+        for (int i = 0; i < cnt; i++) {
+            int target = sc.nextInt();
+            if(target==1) {
+                flag = true;
+                result += ++score;
+            }else {
+                flag = false;
+                score =0;
+            }
+        }
+        System.out.println(result);
+    }
+
+    @Test
+    public void getRank(){
+        /**
+         * 1. 반복수 입력
+         * 2. 각 값 입력
+         * 3. 이중 반복문으로 각 숫자의 순위를 매김
+         */
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] arr = new int[cnt];
+        for (int i = 0; i < cnt; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int target : arr) {
+            int rank =1;
+            for (int i = 0; i < cnt; i++) {
+                if(target < arr[i]) rank++;
+            }
+            System.out.print(rank+" ");
+        }
+
+
+    }
 }
 
 
