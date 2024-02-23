eabcc72783ab7bcd6a4af98902a4aa7c1c7fcdd4
Sunro Lee <tjsfhdhkd@naver.com>
2024-02-23 22:31:38 +0900

    2차배열 문제 풀이 - 삼중 for문으로 해결

--- Java/CordingTest/RecordTest/src/main/java/org/example/Main.java
+++ Java/CordingTest/RecordTest/src/main/java/org/example/Main.java
@@ -4,38 +4,41 @@ import java.util.*;
 
 
 public class Main {
-       int[] dx = {-1, 0 , 1 , 0};
-       int[] dy = {0 , 1, 0 , -1};
-    public int solution(int n, int[][] arr){
-     int answer =0;
-     for(int i=0; i<n;i++){
-         for(int j=0 ; j<n; j++){
-             boolean flag = true;
-             for(int k =0; k<4; k++){
-                 int nx = i + dx[k];
-                 int ny = j + dy[k];
-                 if( nx>=0 && nx<n && ny >=0 && ny<n &&arr[nx][ny] >= arr[i][j]) {
-                     flag = false;
-                     break;
-                 }
-             }
-             if(flag) answer++;
-         }
-     }
-        return answer;
-    }
     public static void main(String[] args) {
-       Main T = new Main();
-       Scanner sc = new Scanner(System.in);
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
                 arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println(T.solution(n,arr));
-
+        System.out.println(T.solution(n,m,arr));
     }
 
+    private int solution(int n, int m, int[][] arr) {
+        int answer=0;
+        for(int i=1; i<=n ;i++){
+            for(int j=1;j<=n;j++){
+                int cnt=0;
+                for (int k = 0; k < m; k++) {
+                    int pi=0, pj=0;
+                    for (int s = 0; s < n; s++) {
+                        if(arr[k][s] ==i) pi=s;
+                        if(arr[k][s] == j) pj=s;
+                    }
+                    if(pi<pj) cnt++;
+                }
+                if (cnt == m) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
 }
+
+
+
