8af05a123de3e461e048015530d3ec05021e066d
Sunro Lee <tjsfhdhkd@naver.com>
2024-02-24 21:41:17 +0900

    기초설정파일

---
+++ Java/Spring Security [Udemy]/springsecurirtybasic/src/test/java/com/eazybytes/springsecurirtybasic/SpringsecurirtybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecurirtybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurirtybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
