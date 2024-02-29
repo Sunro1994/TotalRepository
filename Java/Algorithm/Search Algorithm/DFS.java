abed26a5461ede2a5ada4a0b7f7e1f3143759d0a
Sunro Lee <tjsfhdhkd@naver.com>
2024-02-29 10:36:45 +0900

    DFS, BFS 문제풀이 ( 연결 가지 수 계산 및 미로 문제)

--- Java/Algorithm/Search Algorithm/DFS.java
+++ Java/Algorithm/Search Algorithm/DFS.java
@@ -1,64 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class test2 {
-    static ArrayList<Integer>[] A;
+public class DFS {
+    //방문 배열
     static boolean[] visited;
-static int[] result;
-static int cnt = 1;
+    //인접 리스트
+    static ArrayList<Integer>[] A;
+    //리스트 순서 출력
+    static List<Integer> arr;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //노드의 개수
         int n = Integer.parseInt(st.nextToken());
+        //에지의 개수
         int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-
-        A = new ArrayList[n+1];
+        arr = new ArrayList<>(n);
         visited = new boolean[n+1];
-        result = new int[n + 1];
-
-
-
-
+        A = new ArrayList[n + 1];
         for (int i = 1; i < n + 1; i++) {
+            //인접리스트배열 초기화
             A[i] = new ArrayList<Integer>();
         }
-
-        for (int i = 0; i < m; i++) {
+        //인접 리스트에 그래프 데이터 저장
+        for (int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             A[s].add(e);
             A[e].add(s);
         }
-        int cnt = 0;
-        if (!visited[v]) {
-            DFS(v);
-        }
-        for (int i = 1; i < n+1; i++) {
-            System.out.println(result[i]);
-        }
+        //개수를 판단할 count변수
+        int count =0;
 
-    }
-
-    private static void DFS(int v) {
-        if(visited[v]) return;
-
-        visited[v] = true;
-        result[v] = cnt++;
-        Collections.sort(A[v], Collections.reverseOrder());
-        for (Integer i : A[v]) {
-            if (!visited[i]) {
+        //방문배열에 등록되지 않은 노드를 재귀함수를 통해 찾아냄
+        for (int i = 1; i <n+1; i++) {
+            if(!visited[i]) {
+                count++;
                 DFS(i);
             }
         }
+        System.out.println(count);
+        System.out.println(arr.toString());
+    }
 
+    private static void DFS(int v) {
+        if(visited[v]){
+            return;
+        }
+        visited[v] = true;
+
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                arr.add(i);
+                DFS(i);
+            }
+        }
     }
 }
--- Java/Algorithm/Search Algorithm/DFS2.java
+++ Java/Algorithm/Search Algorithm/DFS2.java
@@ -1,66 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class test {
-    //방문 배열
-    static boolean[] visited;
-    //인접 리스트
+public class DFS2 {
     static ArrayList<Integer>[] A;
-    //리스트 순서 출력
-    static List<Integer> arr;
+    static boolean[] visited;
+static int[] result;
+static int cnt = 1;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        //노드의 개수
         int n = Integer.parseInt(st.nextToken());
-        //에지의 개수
         int m = Integer.parseInt(st.nextToken());
-        arr = new ArrayList<>(n);
+        int v = Integer.parseInt(st.nextToken());
+
+        A = new ArrayList[n+1];
         visited = new boolean[n+1];
-        A = new ArrayList[n + 1];
+        result = new int[n + 1];
+
+
+
+
         for (int i = 1; i < n + 1; i++) {
-            //인접리스트배열 초기화
             A[i] = new ArrayList<Integer>();
         }
-        //인접 리스트에 그래프 데이터 저장
-        for (int i=0; i<m; i++){
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             A[s].add(e);
             A[e].add(s);
         }
-        //개수를 판단할 count변수
-        int count =0;
-
-        //방문배열에 등록되지 않은 노드를 재귀함수를 통해 찾아냄
-        for (int i = 1; i <n+1; i++) {
-            if(!visited[i]) {
-                count++;
-                DFS(i);
-            }
+        int cnt = 0;
+        if (!visited[v]) {
+            DFS(v);
         }
-        System.out.println(count);
-        System.out.println(arr.toString());
+        for (int i = 1; i < n+1; i++) {
+            System.out.println(result[i]);
+        }
+
     }
 
     private static void DFS(int v) {
-        if(visited[v]){
-            return;
-        }
-        visited[v] = true;
+        if(visited[v]) return;
 
-        for (int i : A[v]) {
+        visited[v] = true;
+        result[v] = cnt++;
+        Collections.sort(A[v], Collections.reverseOrder());
+        for (Integer i : A[v]) {
             if (!visited[i]) {
-                arr.add(i);
                 DFS(i);
             }
         }
+
     }
 }
