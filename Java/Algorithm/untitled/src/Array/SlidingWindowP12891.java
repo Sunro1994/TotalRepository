b8d46172eebfce05df75bf921f714dcf1cf9d398
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-14 23:13:41 +0900

    슬라이딩윈도우 백준 12891

---
+++ Java/Algorithm/untitled/src/Array/SlidingWindowP12891.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SlidingWindowP12891 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+        int Result =0;
+        int checkArr[] = new int[4];
+        int myArr[] = new int[4];
+        char A[] = new char[S];
+        int checkSecret = 0;
+
+        A = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+         
+
+    }
+}
