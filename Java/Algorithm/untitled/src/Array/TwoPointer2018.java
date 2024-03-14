72463a890bde44d6624b4c638b371ffdffee56b9
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-14 23:13:29 +0900

    투포인터 백준2018

---
+++ Java/Algorithm/untitled/src/Array/TwoPointer2018.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.Scanner;
+
+public class TwoPointer2018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int cnt =1;
+        int start_idx=1;
+        int end_idx=1;
+        int sum =1;
+
+        while (end_idx != N) {
+            if(sum==N) {
+                cnt++;
+                end_idx++;
+                sum += end_idx;
+            } else if (sum > N) {
+                sum -= start_idx;
+                start_idx++;
+            }else{
+                end_idx++;
+                sum += end_idx;
+            }
+
+        }
+            System.out.println(cnt);
+    }
+}
