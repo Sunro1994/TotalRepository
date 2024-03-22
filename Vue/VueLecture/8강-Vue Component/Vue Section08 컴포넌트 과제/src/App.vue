e22f884e778d3ac79a450c5145d015541210d4d1
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-22 16:37:33 +0900

    Vue Component활용 과제 완료

---
+++ Vue/VueLecture/8강-Vue Component/Vue Section08 컴포넌트 과제/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <section id="app">
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @set-data="setUserData"></user-data>
+  </section>
+</template>
+
+
+<script>
+import ActiveUser from './components/ActiveUser.vue'
+import UserData from './components/UserData.vue'
+    export default{
+  components: { ActiveUser, UserData },
+  data(){
+    return{
+      user:{
+        name: 'sunro',
+        age: 31
+      }
+    }
+  },
+  methods:{
+    setUserData(name,age){
+        this.user = {
+          name: name,
+          age: +age
+        };
+    }
+  }
+
+    }
+</script>
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 20px;
+  border: 1px solid #ccc;
+}
+
+</style>
---
+++ Vue/VueLecture/8강-Vue Component/Vue Section08 컴포넌트 과제/src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="Your name" v-model="enteredName">
+            <input type="text" placeholder="Your age" v-model="enteredAge">
+            <button>Set User Data</button>
+        </form>
+    </section>
+</template>
+
+
+<script>
+export default {
+    emits: ['set-data'],
+    data(){
+        return {
+            enteredName: '',
+            enteredAge:''
+        }
+    },
+    methods:{
+        submitData(){
+            this.$emit('set-data',this.enteredName,this.enteredAge);
+        }
+    }
+}
+</script>
+
+<style>
+section {
+    margin: 2rem auto;
+    max-width: 40rem;
+    border-radius: 20px;
+    border: 1px solid #ccc;
+}
+</style>
