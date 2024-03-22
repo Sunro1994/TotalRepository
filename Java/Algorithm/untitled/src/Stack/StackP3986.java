1bb877c575636511353e3f6746ba885c9fac7d0f
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-22 16:41:42 +0900

    백준 3986 스택문제

--- Java/Algorithm/untitled/src/Stack/StackP3986.java
+++ Java/Algorithm/untitled/src/Stack/StackP3986.java
@@ -1,6 +1,5 @@
 package Stack;
 
-import java.beans.JavaBean;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -8,16 +7,16 @@ public class StackP3986 {
     static int N;
     static String target;
     static int cnt;
-    static Stack<Character> st = new Stack<>();
+    static Stack<Character> st;
     public static void main(String[] args) {
         /*
-        *  알파벳이 교차되면 틀린 답
-        * 짝을 짓고 있어야 한다.
-        * 홀수이면 안된다.
+        *
+        *   짝을 짓고 있어야 한다.
+        *   홀수이면 안된다.
         *
         * 슈도코드작성
-        * int N, String target, int cnt
-        * stack st
+        * int N(길이), String target(받을 문자열)  , int cnt(출력할 정답)
+        * stack st(담을 스택공간)
         *
         * N초기화
         * cnt초기화
@@ -29,19 +28,17 @@ public class StackP3986 {
         * }
         *
         *
-        * target을 판단하는 메서드(target){
+        *   target을 판단하는 메서드(target){
         *   첫 글자를 stack에 넣는다.
         *
-        * for(target 의 1번째 인덱스부터 ~target의 마지막인덱스까지){
+        *    for(target의 1번째 인덱스부터 ~target의 마지막인덱스까지){
         *
         *    if(!stack.empty &&target의 i번째 인덱스가 stack의 peek과 같으면){
         *       stack.pop()
         *       count++
-        * }else if (stack.empty) {
-        *     stack.push(i번째 인덱스)
-        * }else  {
-        *       stack.push(target의 i번째인덱스)
-        *    }
+        *    }else {
+        *       stack.push(i번째 인덱스)
+        *   }
         *
         *
         * }
@@ -52,7 +49,8 @@ public class StackP3986 {
 
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-
+        st = new Stack<>();
+        cnt=0;
         for (int i = 0; i < N; i++) {
             target = sc.next();
             if(target.length()%2 ==0){
@@ -72,9 +70,7 @@ public class StackP3986 {
         for (int i = 1; i <target.length() ; i++) {
             if(!st.empty() && st.peek()==target.charAt(i) ){
                 st.pop();
-            } else if (st.empty()) {
-                st.push(target.charAt(i));
-            }else{
+            } else {
                 st.push(target.charAt(i));
             }
         }
