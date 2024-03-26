47896c0869d04266801846c7137b237270748426
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-27 01:33:05 +0900

    백준 P2675 그리디 알고리즘

--- Java/Algorithm/untitled/src/String/StringP2675.java
+++ Java/Algorithm/untitled/src/String/StringP2675.java
@@ -1,2 +1,31 @@
-package String;public class StringP2675 {
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class StringP2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            //N만큼 입력 반복
+            st = new StringTokenizer(br.readLine());
+            int rep = Integer.parseInt(st.nextToken());
+            String target = st.nextToken();
+            String res ="";
+
+            //rep만큼 각 글자를 반복해야 함
+            for (int j = 1; j <= rep*target.length(); j++) {
+                sb.append(target.charAt(j));
+            }
+            
+            
+        }
+
+    }
 }
