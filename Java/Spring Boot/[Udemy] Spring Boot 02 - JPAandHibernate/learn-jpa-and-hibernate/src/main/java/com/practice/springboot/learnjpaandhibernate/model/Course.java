d440b88aaafedd46c7154c299cbcccf7fbd76f66
Sunro Lee <tjsfhdhkd@naver.com>
2024-03-27 23:30:59 +0900

    @Entity와 DB테이블 이름 맵핑 설정, Id어노테이션 추가

--- Java/Spring Boot/[Udemy] Spring Boot 02 - JPAandHibernate/learn-jpa-and-hibernate/src/main/java/com/practice/springboot/learnjpaandhibernate/model/Course.java
+++ Java/Spring Boot/[Udemy] Spring Boot 02 - JPAandHibernate/learn-jpa-and-hibernate/src/main/java/com/practice/springboot/learnjpaandhibernate/model/Course.java
@@ -1,14 +1,20 @@
 package com.practice.springboot.learnjpaandhibernate.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@Entity(name = "mocktable")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Course {
+    @Id
     private Long id;
+
     private String name;
-    private String autrhor;
+    private String author;
 }
