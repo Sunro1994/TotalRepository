11c02630140274b8f3d537ad5fe78e93bf6c5e1b
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-01 16:10:17 +0900

    REST API 호출 test Entity

---
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/helloworld/HellowWorldBean.java
@@ -0,0 +1,23 @@
+package com.sunro.rest.webservices.restfulwebservices.helloworld;
+
+public class HellowWorldBean {
+    private String message;
+    public HellowWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HellowWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
