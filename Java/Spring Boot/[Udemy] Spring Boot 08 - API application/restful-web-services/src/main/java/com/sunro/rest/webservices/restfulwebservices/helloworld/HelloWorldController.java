016efedf83229aa904f3eb95f3f4e4447a0e471b
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-01 16:10:07 +0900

    REST API 호출 testController

---
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.sunro.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HellowWorldBean helloWorldBean(){
+        return new HellowWorldBean("Hellow WorldBean");
+    }
+
+    //Path Parameters
+    // /users/{id}/todos/{id}
+//    {id} 이러한 부분들을 패스 매개변수라고 한다.
+    // /hello-world/path-vbariable/{name}
+
+    @GetMapping(path = "/hello-world/path-variable/{name}") //{name}과 변수명이 동일해야한다.
+    public HellowWorldBean hellowWorldPathVaribale(@PathVariable(value = "name")String name){
+        return new HellowWorldBean(String.format("Hellow World , %s",name));
+    }
+}
