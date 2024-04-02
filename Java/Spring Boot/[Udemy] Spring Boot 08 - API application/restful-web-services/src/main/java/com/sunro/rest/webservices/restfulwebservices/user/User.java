85181b662d308e210b184637e0ecf2b68e66d4e0
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-02 22:31:59 +0900

    JPA용 Entity객체로 변환

--- Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/user/User.java
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/user/User.java
@@ -1,17 +1,26 @@
 package com.sunro.rest.webservices.restfulwebservices.user;
 
 
+import com.sunro.rest.webservices.restfulwebservices.model.Post;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name = "user_details")
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Size(min=2, message = "Name shoudl have at least 2 characters") //최소 2글자
     private String name;
     @Past(message = "Birth Date should be in the past")  //항상 과거의 날짜로 설정되게 추가하기
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     @Override
     public String toString() {
