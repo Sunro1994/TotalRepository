5387215ba7c64c069d5ca2b01f2e909247941ff4
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-02 22:30:43 +0900

    동적,정적 필터링 컨트롤러

---
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/filter/FilteringController.java
@@ -0,0 +1,44 @@
+package com.sunro.rest.webservices.restfulwebservices.filter;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.PropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.sunro.rest.webservices.restfulwebservices.model.SomeBean;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue filtering(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        MappingJacksonValue mjv = new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mjv.setFilters(filters);
+        return mjv;
+    }
+    @GetMapping("/filtering-list")
+        public MappingJacksonValue filteringList(){
+
+            List<SomeBean> beanList = Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
+
+            MappingJacksonValue mjv = new MappingJacksonValue(beanList);
+
+
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mjv.setFilters(filters);
+
+        return mjv;
+
+    }
+}
