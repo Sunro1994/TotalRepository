3b451e282a663ea5bdad35bc96294a956c22fe19
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-06 02:48:59 +0900

    to do list BackEnd 구성실습

---
+++ Java/Spring Boot/[Udemy] Spring Boot 03 - ToDoListProject/ToDoListApplication/src/test/java/com/practice/springboot/ToDoListApplication/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.ToDoListApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
