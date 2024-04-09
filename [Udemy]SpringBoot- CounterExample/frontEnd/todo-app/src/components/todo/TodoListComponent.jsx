a4b05f6e446b2a9d97224b598d79b6dad3928994
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-09 12:55:31 +0900

    useEffect 사용 및 API 통신 메서드 수행

--- [Udemy]SpringBoot- CounterExample/frontEnd/todo-app/src/components/todo/TodoListComponent.jsx
+++ [Udemy]SpringBoot- CounterExample/frontEnd/todo-app/src/components/todo/TodoListComponent.jsx
@@ -1,22 +1,53 @@
+import { useEffect, useState } from "react";
+import { DeleteTodo, retrieveAllTodosForUsername } from "./api/HellowWorldApiService";
 
 export default function ListTodosComponent() {
     const today = new Date();
     const targetDate = new Date(today.getFullYear() + 12 , today.getMonth(), today.getDay());
-  const todos = [
-    {id: 1, description: "Learn AWS", done: false, targetDate:targetDate},
-    {id: 2, description: "Learn Full Stack" , done: false, targetDate:targetDate}]
+
+    const [todos, setTodos] = useState([]);
+    const [message, setMessage] = useState(null);
+
+
+  useEffect(
+    () => refreshTodos(),  []
+  )
+
+  function refreshTodos() {
+    retrieveAllTodosForUsername('in28minutes')
+    .then(resp => {
+      setTodos(resp.data)
+  })
+
+    .catch(err => console.log(err))
+    .finally(console.log('clean'))
+
+  }
+  function deleteTodo(id){
+    DeleteTodo('in28minutes',id)
+    .then(
+      (resp)=> {
+        setMessage(`deleted ${id} is successful`)
+
+      }
+    ).catch(
+      (error) => console.log(error)
+    )
+  }
+
 
   return (
     <div className="container">
       <h1>Things You wnat to Do!</h1>
+      {message && <div className="alert alert-warning">{message}</div>}
       <div>
         <table className="table">
           <thead>
             <tr>
-              <td>id</td>
-              <td>descripton</td>
-              <td>is Done?</td>
-              <td>target Date</td>
+              <th>descripton</th>
+              <th>is Done?</th>
+              <th>target Date</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -24,10 +55,12 @@ export default function ListTodosComponent() {
                 todos.map(
                     todo => (
                         <tr key={todo.id}>
-                        <td>{todo.id}</td>
                         <td>{todo.description}</td>
                         <td>{todo.done.toString()}</td>
-                        <td>{todo.targetDate.toDateString()}</td>
+                        <td>{todo.targetDate.toString()}</td>
+                        <td><button 
+                        className="btn btn-warning" 
+                        onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                         </tr>
                     )
                 )
