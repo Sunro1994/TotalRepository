0482b7f1c199e990f1f0ab232c0622cde1b48d25
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-09 12:57:11 +0900

    API 통신 테스트 및 CrossOrigin 설정

--- Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,17 @@
 package com.sunro.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
 @RestController
+@CrossOrigin(origins = "http://localhost:3000",allowCredentials = "true")
 public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
-        return "Hello World";
+        return "Hello World v2";
     }
 
     @GetMapping(path = "/hello-world-bean")
