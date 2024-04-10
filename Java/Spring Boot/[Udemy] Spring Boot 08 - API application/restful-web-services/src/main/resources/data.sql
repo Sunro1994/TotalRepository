5cf11c0c012052eeb36087f4f4bb2d964a2e3903
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-10 20:37:57 +0900

    insert 쿼리문 저장

---
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into todo(id,username,description,done,target_date) values(10001,'surno','aws',false,current_date);
+insert into todo(id,username,description,done,target_date) values(10002,'surno','java',false,current_date);
+insert into todo(id,username,description,done,target_date) values(10003,'surno','react',false,current_date);
