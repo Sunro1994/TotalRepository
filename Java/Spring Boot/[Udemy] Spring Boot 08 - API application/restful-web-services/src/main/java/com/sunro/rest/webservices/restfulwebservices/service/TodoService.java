ec34ef19049ce4ca75f537e173e854c3b8464514
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-12 15:09:42 +0900

    TodoService

--- Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/service/TodoService.java
+++ Java/Spring Boot/[Udemy] Spring Boot 08 - API application/restful-web-services/src/main/java/com/sunro/rest/webservices/restfulwebservices/service/TodoService.java
@@ -16,11 +16,11 @@ public class TodoService {
     private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(++todosCount, "in28minutes","Get AWS Certified",
+        todos.add(new Todo(++todosCount, "sunro","Get AWS Certified",
                 LocalDate.now().plusYears(10), false ));
-        todos.add(new Todo(++todosCount, "in28minutes","Learn DevOps",
+        todos.add(new Todo(++todosCount, "sunro","Learn DevOps",
                 LocalDate.now().plusYears(11), false ));
-        todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development",
+        todos.add(new Todo(++todosCount, "sunro","Learn Full Stack Development",
                 LocalDate.now().plusYears(12), false ));
     }
 
