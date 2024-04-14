e0871201a2eb74d0dfff9436f0edc33aad958f37
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-14 21:07:21 +0900

    백준 1110 -  구현

--- Java/Algorithm/untitled/src/Simulation/SimulationP1110.java
+++ Java/Algorithm/untitled/src/Simulation/SimulationP1110.java
@@ -1,2 +1,38 @@
-package Simulation;public class Simulation1110 {
-}
+//package Simulation;
+//
+//import java.util.Scanner;
+//
+//public class SimulationP1110 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//
+//        System.out.println(showCycleCount(N));
+//    }
+//
+//    private static int showCycleCount(int target) {
+//        int sum =0;
+//        int whileSum;
+//        int cnt;
+//        while (true) {
+//            //sum과 whileSum이 같으면 종료
+//            if (sum == whileSum) {
+//                return cnt;
+//            }
+//
+//
+//        if (target / 10 == 0) {
+//            sum = target % 10;
+//        } else {
+//            sum = target%10 + target/10;
+//        }
+//
+//        }
+//
+//
+//
+//
+//
+//        return 0;
+//    }
+//}
