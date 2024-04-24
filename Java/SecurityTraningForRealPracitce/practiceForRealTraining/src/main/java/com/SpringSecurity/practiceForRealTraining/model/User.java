eb9276ab102080be3f0de82e4bd7e04ebcb7a817
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-24 19:59:52 +0900

    User Entity

---
+++ Java/SecurityTraningForRealPracitce/practiceForRealTraining/src/main/java/com/SpringSecurity/practiceForRealTraining/model/User.java
@@ -0,0 +1,46 @@
+package com.SpringSecurity.practiceForRealTraining.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name="USERS")
+@Getter
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Column(name=  "email", nullable = false)
+    private String email;
+
+
+    @Column(name = "role",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+
+    private LocalDateTime createdAt;
+
+    private  LocalDateTime updatedAt;
+}
