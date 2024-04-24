4cb18a178db8e18d0e2ddb1cdf692e29b8ca8aa7
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-24 19:57:24 +0900

    User상태 Enum 타입

---
+++ Java/SecurityTraningForRealPracitce/practiceForRealTraining/src/main/java/com/SpringSecurity/practiceForRealTraining/model/UserStatus.java
@@ -0,0 +1,10 @@
+package com.SpringSecurity.practiceForRealTraining.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserStatus {
+    Y,D
+}
