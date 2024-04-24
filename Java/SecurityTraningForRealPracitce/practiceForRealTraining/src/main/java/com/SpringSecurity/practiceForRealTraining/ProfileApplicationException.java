4ebf25df55858ab2e970b4a45df91d5275f293f2
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-24 19:59:00 +0900

    Custom Exception

---
+++ Java/SecurityTraningForRealPracitce/practiceForRealTraining/src/main/java/com/SpringSecurity/practiceForRealTraining/ProfileApplicationException.java
@@ -0,0 +1,18 @@
+package com.SpringSecurity.practiceForRealTraining;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+//@AllArgsConstructor -> 이걸로는 오류가 난다.
+@Getter
+public class ProfileApplicationException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    // 특별한 로직 [message를 가져오는] 생성자가 필요하기 때문에, 직접 생성자를 정의해야 한다.
+    public ProfileApplicationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
