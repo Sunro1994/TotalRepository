1de7accfe687a5b2b80d408508740760c6b1ea01
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-01 22:59:33 +0900

    Websocket에서 발행된 메시지를 redis로 발행하는 컨트롤러

---
+++ Java/Spring Boot/StompPractice/src/main/java/com/stomp/StompPractice/controller/ChatController.java
@@ -0,0 +1,32 @@
+package com.stomp.StompPractice.controller;
+
+
+import com.stomp.StompPractice.dto.ChatMessage;
+import com.stomp.StompPractice.repository.ChatRoomRepository;
+import com.stomp.StompPractice.service.RedisPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    //    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
+
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시지를 처리한다.
+     */
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())){
+            chatRoomRepository.enterChatRoom(message.getRoomId());
+        message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+
+        }
+    //Websocket에 발행된 메시지를 redis로 발행한다.
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+    }
+}
